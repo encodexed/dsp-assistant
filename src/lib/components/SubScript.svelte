00ddac8290a6cb503236aab3fcb0da80abf0c61b
Robbie Gollan <robbiegollan@gmail.com>
2024-12-26 12:55:03 +1100

    Add a subscript component

---
+++ src/lib/components/SubScript.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let { children, color } = $props();
+</script>
+
+<p class={`select-none ${color}`}>
+	{@render children()}
+</p>
+
+<style>
+	.teal {
+		color: rgb(34, 211, 288);
+		filter: drop-shadow(0 0px 1px rgba(34, 211, 288, 1));
+	}
+
+	.orange {
+		color: rgb(251, 191, 36);
+		filter: drop-shadow(0 0px 1px rgba(251, 191, 36, 1));
+	}
+</style>
