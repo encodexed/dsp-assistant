e8e58c01538d0410888870569427f9856f8a9eb7
Robbie Gollan <robbiegollan@gmail.com>
2024-12-27 19:05:48 +1100

    Fix button within a button issue

--- src/lib/components/UI/CollapsibleWrapper.svelte
+++ src/lib/components/UI/CollapsibleWrapper.svelte
@@ -7,15 +7,19 @@
 	};
 </script>
 
-<button type="button" onclick={toggleVisibility} class="no-select w-full border">
-	<div class="flex w-full items-center justify-between px-1">
+<div class="no-select w-full border">
+	<button
+		type="button"
+		onclick={toggleVisibility}
+		class="flex w-full items-center justify-between px-1"
+	>
 		<h6 class="tracking-wide text-gray-300">{heading}</h6>
 		{#if visible}
 			<img class="h-3 w-3" src="/icons/minus.png" alt="" />
 		{:else}
 			<img class="h-3 w-3" src="/icons/plus.png" alt="" />
 		{/if}
-	</div>
+	</button>
 	<div
 		style={visible ? `height: ${h}px;` : ''}
 		class="h-0 overflow-scroll transition-all duration-300"
@@ -24,4 +28,4 @@
 			{@render children()}
 		</div>
 	</div>
-</button>
+</div>
